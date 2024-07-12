e6927b566f3c638454468d6d1adfe7a421ebaff5
Thierry BABAN <70677643+babanthierry94@users.noreply.github.com>
2024-07-12 14:06:35 +0200

    Update ESANet.py

--- models/ESANet.py
+++ models/ESANet.py
@@ -2,9 +2,17 @@ import tensorflow as tf
 
 # https://github.com/TUI-NICR/ESANet/tree/main
 
-class ESANet(tf.keras.Model):
+# @inproceedings{seichter2021efficient,
+#   title={Efficient rgb-d semantic segmentation for indoor scene analysis},
+#   author={Seichter, Daniel and K{\"o}hler, Mona and Lewandowski, Benjamin and Wengefeld, Tim and Gross, Horst-Michael},
+#   booktitle={2021 IEEE international conference on robotics and automation (ICRA)},
+#   pages={13525--13531},
+#   year={2021},
+#   organization={IEEE}
+# }
+
+class ESANet():
     def __init__(self, num_classes=16, input_shape=(512,512,4)):
-        super(ESANet, self).__init__()
         self.input_size = input_shape
         self.inputs = tf.keras.Input(shape=self.input_size)
         
@@ -15,17 +23,17 @@ class ESANet(tf.keras.Model):
         self.dec_module3 = Decoder_module(out_channels=128)
 
         self.conv_1 = tf.keras.Sequential([
-                  tf.keras.layers.Conv2D(512, kernel_size=1, padding='same'),
+                  tf.keras.layers.Conv2D(512, kernel_size=1, padding='same', kernel_initializer='he_normal'),
                   tf.keras.layers.BatchNormalization(),
                   tf.keras.layers.ReLU(),
               ])
         self.conv_2 = tf.keras.Sequential([
-                  tf.keras.layers.Conv2D(256, kernel_size=1, padding='same'),
+                  tf.keras.layers.Conv2D(256, kernel_size=1, padding='same', kernel_initializer='he_normal'),
                   tf.keras.layers.BatchNormalization(),
                   tf.keras.layers.ReLU(),
               ])
         self.conv_3 = tf.keras.Sequential([
-                  tf.keras.layers.Conv2D(128, kernel_size=1, padding='same'),
+                  tf.keras.layers.Conv2D(128, kernel_size=1, padding='same', kernel_initializer='he_normal'),
                   tf.keras.layers.BatchNormalization(),
                   tf.keras.layers.ReLU(),
               ])
@@ -33,69 +41,7 @@ class ESANet(tf.keras.Model):
         self.learned_up1 = LearnedUp()
         self.learned_up2 = LearnedUp()
         # Classifier
-        self.classifier = tf.keras.layers.Conv2D(num_classes, kernel_size=3, padding='same')
-
-    def _start_block(self, x, name):
-        outputs = tf.keras.layers.Conv2D(64, kernel_size=7, strides=2, padding='same', name='%s_conv1_conv'%name)(x)
-        outputs = tf.keras.layers.BatchNormalization(name='%s_conv1_bn'%name)(outputs)
-        outputs = tf.keras.layers.Activation("relu", name='%s_conv1_relu'%name)(outputs)
-        # outputs = tf.keras.layers.MaxPool2D(pool_size=3, strides=2, padding='same', name="%s_pool1_pool"%name)(outputs)
-        return outputs
-
-    def _bottleneck_resblock(self, x, filters, stride, dilation_factor, name, identity_connection=True):
-        assert filters % 4 == 0, 'Bottleneck number of output ERROR!'
-        # branch1
-        if not identity_connection:
-            o_b1 = tf.keras.layers.Conv2D(filters, kernel_size=1, strides=stride, padding='same', name='%s_0_conv'%name)(x)
-            o_b1 = tf.keras.layers.BatchNormalization(name='%s_0_bn'%name)(o_b1)
-        else:
-            o_b1 = x
-        # branch2
-        o_b2a = tf.keras.layers.Conv2D(filters / 4, kernel_size=1, strides=1, padding='same', name='%s_1_conv'%name)(x)
-        o_b2a = tf.keras.layers.BatchNormalization(name='%s_1_bn'%name)(o_b2a)
-        o_b2a = tf.keras.layers.Activation("relu", name='%s_1_relu'%name)(o_b2a)
-
-        o_b2b = tf.keras.layers.Conv2D(filters / 4, kernel_size=3, strides=stride, dilation_rate=dilation_factor, padding='same', name='%s_2_conv'%name)(o_b2a)
-        o_b2b = tf.keras.layers.BatchNormalization(name='%s_2_bn'%name)(o_b2b)
-        o_b2b = tf.keras.layers.Activation("relu", name='%s_2_relu'%name)(o_b2b)
-
-        o_b2c = tf.keras.layers.Conv2D(filters, kernel_size=1, strides=1, padding='same', name='%s_3_conv'%name)(o_b2b)
-        o_b2c = tf.keras.layers.BatchNormalization(name='%s_3_bn'%name)(o_b2c)
-
-        # add
-        outputs = tf.keras.layers.Add(name='%s_add'%name)([o_b1, o_b2c])
-        # relu
-        outputs = tf.keras.layers.Activation("relu", name='%s_out'%name)(outputs)
-        return outputs
-
-    def _load_pretrained(self, model):
-        def remove_prefix(string):
-            if string.startswith("rgb_"):
-                return string[len("rgb_"):]
-            elif string.startswith("depth_"):
-                return string[len("depth_"):]
-            else:
-                return string
-
-        weight_model = tf.keras.applications.ResNet50(weights="imagenet", include_top=False, input_shape=(self.input_size[0], self.input_size[1], 3))
-
-        for l in model.layers:
-            layer_name = l.name
-            print(layer_name)
-            if layer_name.startswith(("rgb_", "depth_")):
-                layer_name_no_prefix = remove_prefix(layer_name)
-                try:
-                    if layer_name_no_prefix in [layer.name for layer in weight_model.layers]:
-                        weight_layer = weight_model.get_layer(layer_name_no_prefix)
-                        l.set_weights(weight_layer.get_weights())
-                        # print(f"Loaded weights for layer: {layer_name} (mapped to {layer_name_no_prefix})")
-                    else:
-                        print(f"No matching layer found in pre-trained model for layer: {layer_name}")
-                except Exception as e:
-                    print(f"Error loading weights for layer {layer_name}: {str(e)}")
-            else:
-                print(f"Skipping layer: {layer_name} (not prefixed with 'rgb_' or 'depth_')")
-        return model
+        self.classifier = tf.keras.layers.Conv2D(num_classes, kernel_size=3, padding='same', kernel_initializer='he_normal')
 
     def __call__(self):
         # ResNet50
@@ -105,8 +51,7 @@ class ESANet(tf.keras.Model):
 
         # Encoder
         rgb_input = self.inputs[:,:,:,:3]
-        depth = tf.expand_dims(self.inputs[:,:,:,3], axis=-1)
-        depth_input = tf.concat([depth, depth, depth], -1)
+        depth_input = tf.expand_dims(self.inputs[:,:,:,3], axis=-1)
 
         ################### Depth Branch ##############################
         #starter_block
@@ -173,8 +118,69 @@ class ESANet(tf.keras.Model):
 
         model = tf.keras.Model(inputs=self.inputs, outputs=output, name='ESANet')
         model = self._load_pretrained(model)
-
         return model
+
+    def _start_block(self, x, name):
+        outputs = tf.keras.layers.Conv2D(64, kernel_size=7, strides=2, padding='same', name='%s_conv1_conv'%name)(x)
+        outputs = tf.keras.layers.BatchNormalization(name='%s_conv1_bn'%name)(outputs)
+        outputs = tf.keras.layers.Activation("relu", name='%s_conv1_relu'%name)(outputs)
+        # outputs = tf.keras.layers.MaxPool2D(pool_size=3, strides=2, padding='same', name="%s_pool1_pool"%name)(outputs)
+        return outputs
+
+    def _bottleneck_resblock(self, x, filters, stride, dilation_factor, name, identity_connection=True):
+        assert filters % 4 == 0, 'Bottleneck number of output ERROR!'
+        # branch1
+        if not identity_connection:
+            o_b1 = tf.keras.layers.Conv2D(filters, kernel_size=1, strides=stride, padding='same', name='%s_0_conv'%name)(x)
+            o_b1 = tf.keras.layers.BatchNormalization(name='%s_0_bn'%name)(o_b1)
+        else:
+            o_b1 = x
+        # branch2
+        o_b2a = tf.keras.layers.Conv2D(filters / 4, kernel_size=1, strides=1, padding='same', name='%s_1_conv'%name)(x)
+        o_b2a = tf.keras.layers.BatchNormalization(name='%s_1_bn'%name)(o_b2a)
+        o_b2a = tf.keras.layers.Activation("relu", name='%s_1_relu'%name)(o_b2a)
+
+        o_b2b = tf.keras.layers.Conv2D(filters / 4, kernel_size=3, strides=stride, dilation_rate=dilation_factor, padding='same', name='%s_2_conv'%name)(o_b2a)
+        o_b2b = tf.keras.layers.BatchNormalization(name='%s_2_bn'%name)(o_b2b)
+        o_b2b = tf.keras.layers.Activation("relu", name='%s_2_relu'%name)(o_b2b)
+
+        o_b2c = tf.keras.layers.Conv2D(filters, kernel_size=1, strides=1, padding='same', name='%s_3_conv'%name)(o_b2b)
+        o_b2c = tf.keras.layers.BatchNormalization(name='%s_3_bn'%name)(o_b2c)
+
+        # add
+        outputs = tf.keras.layers.Add(name='%s_add'%name)([o_b1, o_b2c])
+        # relu
+        outputs = tf.keras.layers.Activation("relu", name='%s_out'%name)(outputs)
+        return outputs
+
+    def _load_pretrained(self, model):
+        def remove_prefix(string):
+            if string.startswith("rgb_"):
+                return string[len("rgb_"):]
+            elif string.startswith("depth_"):
+                return string[len("depth_"):]
+            else:
+                return string
+
+        weight_model = tf.keras.applications.ResNet50(weights="imagenet", include_top=False, input_shape=(self.input_size[0], self.input_size[1], 3))
+
+        for l in model.layers:
+            layer_name = l.name
+            if layer_name.startswith(("rgb_", "depth_")):
+                layer_name_no_prefix = remove_prefix(layer_name)
+                try:
+                    if layer_name_no_prefix in [layer.name for layer in weight_model.layers]:
+                        weight_layer = weight_model.get_layer(layer_name_no_prefix)
+                        l.set_weights(weight_layer.get_weights())
+                        # print(f"Loaded weights for layer: {layer_name} (mapped to {layer_name_no_prefix})")
+                    else:
+                        print(f"No matching layer found in pre-trained model for layer: {layer_name}")
+                except Exception as e:
+                    print(f"Error loading weights for layer {layer_name}: {str(e)}")
+            # else:
+            #     print(f"Skipping layer: {layer_name} (not prefixed with 'rgb_' or 'depth_')")
+        return model
+
 
 
 class ContextModule(tf.keras.layers.Layer):
@@ -193,7 +199,6 @@ class ContextModule(tf.keras.layers.Layer):
                 bins=bins,
                 activation=activation,
                 upsampling_mode=upsampling_mode)
-
         else :
               raise NotImplementedError("%s Not Implemented."%context_module_name)
 
@@ -211,14 +216,12 @@ class AdaptiveAvgPool2D(tf.keras.layers.Layer):
 
         target_height = self.output_size[0]
         target_width = self.output_size[1]
-
+        
         stride_height = height // target_height
         stride_width = width // target_width
-
         # Calculate pooling size for height and width
         pool_size_height = height - (target_height - 1) * stride_height
         pool_size_width = width - (target_width - 1) * stride_width
-
         # Perform average pooling
         pooled_output = tf.nn.avg_pool(inputs, ksize=[1, pool_size_height, pool_size_width, 1],
                                        strides=[1, stride_height, stride_width, 1], padding='VALID')
@@ -236,13 +239,13 @@ class PyramidPoolingModule(tf.keras.layers.Layer):
             self.features.append(tf.keras.Sequential([
                 # tf.keras.layers.GlobalAveragePooling2D(keepdims=True),
                 AdaptiveAvgPool2D(output_size=(bin_size, bin_size)),
-                tf.keras.layers.Conv2D(reduction_dim, kernel_size=1, padding='same'),
+                tf.keras.layers.Conv2D(reduction_dim, kernel_size=1, padding='same', kernel_initializer='he_normal'),
                 tf.keras.layers.BatchNormalization(),
                 activation
             ]))
-
+            
         self.final_conv = tf.keras.Sequential([
-                  tf.keras.layers.Conv2D(out_dim, kernel_size=1, padding='same'),
+                  tf.keras.layers.Conv2D(out_dim, kernel_size=1, padding='same', kernel_initializer='he_normal'),
                   tf.keras.layers.BatchNormalization(),
                   activation,
               ])
@@ -280,14 +283,13 @@ class NonBottleneck1D(tf.keras.layers.Layer):
     def __init__(self, out_channels, stride=1, downsample=None, groups=1, base_width=None, dilation=1, norm_layer=None,
                  activation=tf.keras.layers.ReLU(), residual_only=False):
         super(NonBottleneck1D, self).__init__()
-        # warnings.warn('parameters groups, base_width and norm_layer are '
-        #               'ignored in NonBottleneck1D')
-        self.conv3x1_1 = tf.keras.layers.Conv2D(out_channels, (3, 1), strides=(stride, 1), padding='same', use_bias=True)
-        self.conv1x3_1 = tf.keras.layers.Conv2D(out_channels, (1, 3), strides=(1, stride), padding='same', use_bias=True)
+        # warnings.warn('parameters groups, base_width and norm_layer are ignored in NonBottleneck1D')
+        self.conv3x1_1 = tf.keras.layers.Conv2D(out_channels, (3, 1), strides=(stride, 1), padding='same', use_bias=True, kernel_initializer='he_normal')
+        self.conv1x3_1 = tf.keras.layers.Conv2D(out_channels, (1, 3), strides=(1, stride), padding='same', use_bias=True, kernel_initializer='he_normal')
         self.bn1 = tf.keras.layers.BatchNormalization(epsilon=1e-03)
         self.act = activation
-        self.conv3x1_2 = tf.keras.layers.Conv2D(out_channels, (3, 1), padding='same', use_bias=True, dilation_rate=(dilation, 1))
-        self.conv1x3_2 = tf.keras.layers.Conv2D(out_channels, (1, 3), padding='same', use_bias=True, dilation_rate=(1, dilation))
+        self.conv3x1_2 = tf.keras.layers.Conv2D(out_channels, (3, 1), padding='same', use_bias=True, dilation_rate=(dilation, 1), kernel_initializer='he_normal')
+        self.conv1x3_2 = tf.keras.layers.Conv2D(out_channels, (1, 3), padding='same', use_bias=True, dilation_rate=(1, dilation), kernel_initializer='he_normal')
         self.bn2 = tf.keras.layers.BatchNormalization(epsilon=1e-03)
         self.dropout_rate = 0.5
         self.dropout = tf.keras.layers.Dropout(self.dropout_rate)
@@ -340,7 +342,7 @@ class LearnedUp(tf.keras.layers.Layer):
         # Create the custom initializer with the kernel values
         initializer = CustomInitializer(kernel_values)
         # Define the convolutional layer with specific kernel values (kernel_size=3)
-        self.depth_wise = tf.keras.layers.DepthwiseConv2D(kernel_size=3, kernel_initializer=initializer, padding='same')
+        self.depth_wise = tf.keras.layers.DepthwiseConv2D(kernel_size=3, padding='same', kernel_initializer=initializer)
 
     def __call__(self, x):
         x = self.nearest_up(x)
@@ -351,7 +353,7 @@ class Decoder_module(tf.keras.layers.Layer):
     def __init__(self, out_channels):
         super(Decoder_module, self).__init__()
         self.conv_3x3 = tf.keras.Sequential([
-                  tf.keras.layers.Conv2D(out_channels, kernel_size=1, padding='same'),
+                  tf.keras.layers.Conv2D(out_channels, kernel_size=1, padding='same', kernel_initializer='he_normal'),
                   tf.keras.layers.BatchNormalization(),
                   tf.keras.layers.ReLU(),
               ])
@@ -375,27 +377,27 @@ class SqueezeAndExciteFusionAdd(tf.keras.layers.Layer):
 
         self.se_rgb = tf.keras.Sequential([
             tf.keras.layers.GlobalAveragePooling2D(keepdims=True),
-            tf.keras.layers.Conv2D(C // reduction, kernel_size=1, padding='same'),
+            tf.keras.layers.Conv2D(C // reduction, kernel_size=1, padding='same', kernel_initializer='he_normal'),
             tf.keras.layers.ReLU(),
-            tf.keras.layers.Conv2D(C, kernel_size=1, padding='same'),
+            tf.keras.layers.Conv2D(C, kernel_size=1, padding='same', kernel_initializer='he_normal'),
             tf.keras.layers.Activation('sigmoid')
         ])
 
         self.se_depth = tf.keras.Sequential([
             tf.keras.layers.GlobalAveragePooling2D(keepdims=True),
-            tf.keras.layers.Conv2D(C // reduction, kernel_size=1, padding='same'),
+            tf.keras.layers.Conv2D(C // reduction, kernel_size=1, padding='same', kernel_initializer='he_normal'),
             tf.keras.layers.ReLU(),
-            tf.keras.layers.Conv2D(C, kernel_size=1, padding='same'),
+            tf.keras.layers.Conv2D(C, kernel_size=1, padding='same', kernel_initializer='he_normal'),
             tf.keras.layers.Activation('sigmoid')
         ])
 
     def __call__(self, rgb, depth):
         rgb_weight = self.se_rgb(rgb)
         rgb = tf.multiply(rgb, rgb_weight)
-
+        
         depth_weight = self.se_depth(depth)
         depth = tf.multiply(depth, depth_weight)
-
+        
         out = tf.add(rgb, depth)
         return out
 
